5632d4e691eed69e26945962ab280d17459516bc
Semere Tereffe <se.semere.tereffe@gmail.com>
2024-08-20 02:42:33 +0300

    Switch preliminary IP check to only check for mailDomain instead of domain

--- emailwiz.sh
+++ emailwiz.sh
@@ -55,10 +55,10 @@ if [ "$use_cert_config" = "yes" ]; then
 fi
 
 # Preliminary record checks
-ipv4=$(host "$domain" | grep -m1 -Eo '([0-9]+\.){3}[0-9]+')
-[ -z "$ipv4" ] && echo "\033[0;31mPlease point your domain ("$domain") to your server's ipv4 address." && exit 1
-ipv6=$(host "$domain" | grep "IPv6" | awk '{print $NF}')
-[ -z "$ipv6" ] && echo "\033[0;31mPlease point your domain ("$domain") to your server's ipv6 address." && exit 1
+ipv4=$(host "$maildomain" | grep -m1 -Eo '([0-9]+\.){3}[0-9]+')
+[ -z "$ipv4" ] && echo "\033[0;31mPlease point your domain ("$maildomain") to your server's ipv4 address." && exit 1
+ipv6=$(host "$maildomain" | grep "IPv6" | awk '{print $NF}')
+[ -z "$ipv6" ] && echo "\033[0;31mPlease point your domain ("$maildomain") to your server's ipv6 address." && exit 1
 
 # Open required mail ports
 for port in 80 993 465 25 587 110 995; do
